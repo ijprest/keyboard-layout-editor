6467d6161699e92e7f1d052e746de17e42a2174f
Ian Prest <github.com@ian.prest.ca>
2013-10-14 09:47:45 -0400

    Reformatted the deploy script to be easier to read.

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,10 @@
 @echo off
-aws s3 sync . s3://www.keyboard-layout-editor.com --acl public-read --exclude aws-private-key.txt --exclude *.py --exclude mongoose* --exclude .git* --exclude *.bat --exclude upload-policy.txt %*
+aws s3 sync . s3://www.keyboard-layout-editor.com ^
+	--acl public-read ^
+	--exclude aws-private-key.txt ^
+	--exclude *.py ^
+	--exclude mongoose* ^
+	--exclude .git* ^
+	--exclude *.bat ^
+	--exclude upload-policy.txt ^
+	%*
