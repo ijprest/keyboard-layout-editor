c3e6c81d18fbb10984cc62a485da699bd7d600f8
Ian Prest <github.com@ian.prest.ca>
2015-07-05 02:44:18 -0400

    Fixed resizing with shift+arrows
    -- Incorrect call to Math.round

--- kb.js
+++ kb.js
@@ -473,8 +473,8 @@
 			}
 			transaction("size", function() {
 				$scope.selectedKeys.forEach(function(selectedKey) {
-					update(selectedKey, 'width', Math.round10(Math.max(1,selectedKey.width + x,-2)));
-					update(selectedKey, 'height', Math.round10(Math.max(1,selectedKey.height + y,-2)));
+					update(selectedKey, 'width', Math.round10(Math.max(1,selectedKey.width + x),-2));
+					update(selectedKey, 'height', Math.round10(Math.max(1,selectedKey.height + y),-2));
 					renderKey(selectedKey);
 				});
 				$scope.multi = angular.copy($scope.selectedKeys.last());
