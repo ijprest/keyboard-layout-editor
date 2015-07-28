6797c23937f414c8710a1d00b99a97ae9705eb86
iandoug <ian@zti.co.za>
2015-07-28 09:16:30 +0200

    Added code and data for supporting the backgrounds

--- kb.js
+++ kb.js
@@ -169,6 +169,28 @@
 			$scope.samples = data.samples;
 		});
 
+		// Known backgrounds
+		$scope.backgrounds = {}; //????
+		$scope.carbonfibre = {};
+		$scope.leather = {};
+		$scope.marble = {};
+		$scope.metal = {};
+		$scope.plasticABSwhite = {};
+		$scope.plasticPBTbrown_black = {};
+		$scope.plasticPBTwhite = {};
+		$scope.wood = {};
+		$http.get('backgrounds.json').success(function(data) {
+		  $scope.carbonfibre = data.carbonfibre;
+		  $scope.leather = data.leather;
+		  $scope.marble = data.marble;
+		  $scope.metal = data.metal;
+		  $scope.plasticABSwhite = data.plasticABSwhite;
+		  $scope.plasticPBTbrown_black = data.plasticPBTbrown_black;
+		  $scope.plasticPBTwhite = data.plasticPBTwhite;
+		  $scope.wood = data.wood;
+		});
+		
+		
 		// The currently selected palette
 		$scope.palette = {};
 
