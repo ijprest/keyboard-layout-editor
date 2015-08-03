6a33225661ecfe83bf50ba73b545c001e6c5ae30
Ian Prest <github.com@ian.prest.ca>
2015-08-02 22:38:04 -0400

    Updated deploy script with OAuth page

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,7 @@ if "%1"=="" SET PARMS=--dryrun
 for %%Q IN (css js fonts samples) DO (
   aws s3 sync .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
 )
-for %%Q IN (kb.html kb.js render.js serial.js extensions.js *.md *.json favicon.ico) DO (
+for %%Q IN (kb.html oauth.html kb.js render.js serial.js extensions.js *.md *.json favicon.ico) DO (
   aws s3 cp .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
 )
 goto :EOF
