0ea1dab1eec00d5ceb38148be75761abe88b4911
Ian Prest <github.com@ian.prest.ca>
2015-08-03 19:54:39 -0400

    Updated deployment script for 'bg' folder

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 set PARMS=
 if "%1"=="" SET PARMS=--dryrun
-for %%Q IN (css js fonts samples) DO (
+for %%Q IN (css js fonts samples bg) DO (
   aws s3 sync .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
 )
 for %%Q IN (kb.html oauth.html kb.js render.js serial.js extensions.js *.md *.json favicon.ico) DO (
