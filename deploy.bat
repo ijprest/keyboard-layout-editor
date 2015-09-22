ebc13ea0d20a3ad215ae83ba498bcc3badfd88fd
Ian Prest <github.com@ian.prest.ca>
2015-09-21 23:36:48 -0400

    Tweaked deploy script to allow me to deploy a single file easily

--- deploy.bat
+++ deploy.bat
@@ -2,10 +2,14 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 set PARMS=
 if "%1"=="" SET PARMS=--dryrun
-for %%Q IN (css js fonts samples bg) DO (
-  aws s3 sync .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
-)
-for %%Q IN (kb.html oauth.html kb.js render.js serial.js extensions.js *.md *.json favicon.ico) DO (
-  aws s3 cp .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
+if "%1"=="*" (
+	for %%Q IN (css js fonts samples bg) DO (
+		aws s3 sync .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
+	)
+	for %%Q IN (kb.html oauth.html kb.js render.js serial.js extensions.js *.md *.json favicon.ico) DO (
+		aws s3 cp .\%%Q s3://www.keyboard-layout-editor.com/%%Q --acl public-read %PARMS%
+	)
+) else (
+	aws s3 cp %~1 s3://www.keyboard-layout-editor.com/%~1 --acl public-read %PARMS%
 )
 goto :EOF
